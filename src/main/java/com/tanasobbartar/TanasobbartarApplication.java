3cedffe6908984a5afc21ea113bef92f3ec19e24
farham <famms.shirazi@gmail.com>
2026-05-06 01:18:54 +0200

    initializing project enviroment

---
+++ src/main/java/com/tanasobbartar/TanasobbartarApplication.java
@@ -0,0 +1,13 @@
+package com.tanasobbartar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanasobbartarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanasobbartarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanasobbartar/TanasobbartarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanasobbartar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanasobbartarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
