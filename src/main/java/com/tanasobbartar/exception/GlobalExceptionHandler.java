e3ab4bbc31965992b969b80dad18b8bc1ad89955
farham <famms.shirazi@gmail.com>
2026-05-07 04:01:13 +0200

    adding exception capability and messages file

---
+++ src/main/java/com/tanasobbartar/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package ir.fathi.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Autowired
+    private MessageSourceAccessor messageSourceAccessor;
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiException> handleRuntimeException(RuntimeException ex,
+                                                               HttpServletRequest request) {
+
+        ApiException exception =
+                new ApiException(400, messageSourceAccessor.getMessage(ex.getMessage()),
+                        request.getRequestURI(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception);
+    }
+}
